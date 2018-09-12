717dd2fcfd4eb8a8083130761ad1c3b420b2573f
Katie <katie.m.dempsey@gmail.com>
2018-09-13 11:39:06 +1200

    Build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm *.class # clean project
-javac Calc.java # compile project
-if[$?-ne0] #if exit code is not 0 javac failed
+rm *.class
+
+javac Calc.java
+if [ $? -ne 0 ]
 then
-  echo "Compile failed!"
+  echo "Compile failed"
   exit 1
-fi
-java -ea Calc # run test
+fijava -ea Calc
